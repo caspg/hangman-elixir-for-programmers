f1dc4f837cb93c7e8364629cacc4216f38bf5a9d
Kacper Golinski <kacper.golinski@gmail.com>
2017-09-17 22:47:08 +0200

    add Agent to Dictionary.WordList

--- dictionary/lib/dictionary/word_list.ex
+++ dictionary/lib/dictionary/word_list.ex
@@ -1,13 +1,16 @@
 defmodule Dictionary.WordList do
+  def start_link do
+    Agent.start_link(&start/0)
+  end
+
+  def random_word(agent) do
+    Agent.get(agent, &Enum.random/1)
+  end
+
   def start do
     "../../assets/words.txt"
     |> Path.expand(__DIR__)
     |> File.read!()
     |> String.split(~r/\n/)
-  end
-
-  def random_word(word_list) do
-    word_list
-    |> Enum.random()
   end
 end
